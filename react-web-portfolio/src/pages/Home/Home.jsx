c1b5dcaeb85c7981d485208feb4b9a7b440df3ee
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-14 12:58:49 -0300

    feat: adding home page

---
+++ react-web-portfolio/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { NavBar } from '../../components/NavBar';
+import { Banner } from '../../components/Banner';
+
+const HomePage = () => {
+    return (
+        <div className="app">
+            <NavBar />
+            <Banner />
+        </div>
+    )
+}
+
+export default HomePage;
