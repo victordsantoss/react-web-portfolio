d8ac739ab7a1129f73ccff803305cff6759223f7
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-14 13:01:20 -0300

    chore: adicionando arquivos de configuração

---
+++ react-web-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ApplicationRoutes from "./routes";
+
+function App() {
+  return (
+    <div className="App">
+      <ApplicationRoutes />
+    </div>
+  );
+}
+
+export default App;
