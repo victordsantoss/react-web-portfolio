7c6db935de7ebbe65463d54da2f9ca2640559743
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-15 00:53:34 -0300

    feat: adicionando seção de habilidades na homepage

--- react-web-portfolio/src/pages/Home/Home.jsx
+++ react-web-portfolio/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import { NavBar } from '../../components/NavBar';
 import { Banner } from '../../components/Banner';
+import { Skills } from '../../components/Skills';
 
 const HomePage = () => {
     return (
         <div className="app">
             <NavBar />
             <Banner />
+            <Skills />
         </div>
     )
 }
