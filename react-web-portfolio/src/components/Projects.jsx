2985d4e248b56a4cbb58e145e4a44f6f001b5806
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-15 13:36:40 -0300

    styles: adicionando linha

--- react-web-portfolio/src/components/Projects.jsx
+++ react-web-portfolio/src/components/Projects.jsx
@@ -16,7 +16,7 @@ export const Projects = () => {
                             {({ isVisible }) =>
                                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                                     <h2>Projetos</h2>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                    <p className="line"></p>
                                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
                                         <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                                             <Tab.Pane eventKey="first">
@@ -33,7 +33,7 @@ export const Projects = () => {
                                                     }
                                                 </Row>
                                             </Tab.Pane>
-    
+
                                         </Tab.Content>
                                     </Tab.Container>
                                 </div>}
