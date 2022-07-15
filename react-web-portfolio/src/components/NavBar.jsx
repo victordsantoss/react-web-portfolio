51f98b7d01cad9154c2d3aaa06ab800a49838d37
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-15 00:51:16 -0300

    refactor: modularizando imports

--- react-web-portfolio/src/components/NavBar.jsx
+++ react-web-portfolio/src/components/NavBar.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { IoLogoXing } from 'react-icons/io';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
-import navIcon4 from '../assets/img/nav-icon4.svg';
+import { navbarIcons } from "../utils/navbar-icons";
 
 export const NavBar = () => {
 
@@ -38,14 +35,14 @@ export const NavBar = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Sobre</Nav.Link>
+                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Habilidades</Nav.Link>
                         <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projetos</Nav.Link>
                     </Nav>
                     <span className="navbar-text d-flex justify-content-around">
                         <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/victorsamuelengenharia/" target="_blank"><img src={navIcon1} alt="" /></a>
-                            <a href="https://github.com/victordsantoss" target="_blank"><img src={navIcon4} alt="" /></a>
-                            <a href="https://www.instagram.com/victordsantoss/" target="_blank"><img src={navIcon3} alt="" /></a>
+                            <a href="https://www.linkedin.com/in/victorsamuelengenharia/" target="_blank" rel="noreferrer" ><img src={navbarIcons.linkedin} alt="" /></a>
+                            <a href="https://github.com/victordsantoss" target="_blank" rel="noreferrer" ><img src={navbarIcons.instagram} alt="" /></a>
+                            <a href="https://www.instagram.com/victordsantoss/" target="_blank" rel="noreferrer" ><img src={navbarIcons.github} alt="" /></a>
                         </div>
                     </span>
                 </Navbar.Collapse>
