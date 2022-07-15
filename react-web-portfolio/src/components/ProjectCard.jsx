c4f2577469a8b58e3e2fbd3e419adc4483c7cc8b
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-15 02:04:57 -0300

    feat: adicioanndo card de projeto

---
+++ react-web-portfolio/src/components/ProjectCard.jsx
@@ -0,0 +1,15 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl, link }) => {
+    return (
+        <Col size={12} sm={6} md={4}>
+            <div className="proj-imgbx">
+                <img src={imgUrl} width='150px' height={500} />
+                <div className="proj-txtx">
+                    <a href={link} className="link-project" target='_blank'><h4>{title}</h4></a>
+                    <span>{description}</span>
+                </div>
+            </div>
+        </Col>
+    )
+}
