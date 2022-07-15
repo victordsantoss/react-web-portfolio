e66491fc2929854563ef201033059f5aea0a2104
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-15 02:01:50 -0300

    utils: adicionando constantes globais

---
+++ react-web-portfolio/src/utils/projects-constants.js
@@ -0,0 +1,45 @@
+import groovooTicketing from '../assets/img/projects/groovoo-ticketing.png';
+import groovooSocialNetworking from '../assets/img/projects/groovoo-social-networking.jpeg';
+import groovooOrganizer from '../assets/img/projects/groovoo-organizer.jpeg';
+import concursoDeRedacao from '../assets/img/projects/dpu-concurso-de-redacao.png';
+import gaiadex from '../assets/img/projects/gaiadex.png';
+import portalDpu from '../assets/img/projects/portal-dpu.png';
+
+export const projects = [
+    {
+        title: 'Groovoo Ticketing',
+        description: 'Desenvolvedor Full stack',
+        imgUrl: groovooTicketing,
+        link: 'https://groovooapp.com/'
+    },
+    {
+        title: 'Groovoo Social Networking',
+        description: 'Desenvolvedor Full stack',
+        imgUrl: groovooSocialNetworking,
+        link: 'https://apps.apple.com/br/app/groovoo-social-networking/id1529056214'
+    },
+    {
+        title: 'Groovoo Ticketing',
+        description: 'Desenvolvedor Full stack',
+        imgUrl: groovooOrganizer,
+        link: 'https://apps.apple.com/br/app/groovoo-organizer/id1593068649'
+    },
+    {
+        title: 'Concurso de Redação da Defensoria Pública da União',
+        description: 'Desenvolvedor fron-end',
+        imgUrl: concursoDeRedacao,
+        link: 'https://concursoderedacao.dpu.def.br/'
+    },
+    {
+        title: 'GaiaDex',
+        description: 'Desenvolvedor Full stack',
+        imgUrl: gaiadex,
+        link: '#'
+    },
+    {
+        title: 'Portal DPU',
+        description: 'Desenvolvedor front end',
+        imgUrl: portalDpu,
+        link: 'https://www.dpu.def.br/'
+    }
+]
