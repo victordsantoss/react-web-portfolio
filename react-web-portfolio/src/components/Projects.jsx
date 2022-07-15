31e4f6776f234dcb0d6e887343282e437a8edfd9
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-15 02:04:32 -0300

    feat: adicioanndo seção de projetos

---
+++ react-web-portfolio/src/components/Projects.jsx
@@ -0,0 +1,48 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import TrackVisibility from 'react-on-screen';
+
+import { projects } from "../utils/projects-constants"
+
+export const Projects = () => {
+
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col size={12}>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                    <h2>Projetos</h2>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                                        <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                                            <Tab.Pane eventKey="first">
+                                                <Row>
+                                                    {
+                                                        projects.map((project, index) => {
+                                                            return (
+                                                                <ProjectCard
+                                                                    key={index}
+                                                                    {...project}
+                                                                />
+                                                            )
+                                                        })
+                                                    }
+                                                </Row>
+                                            </Tab.Pane>
+    
+                                        </Tab.Content>
+                                    </Tab.Container>
+                                </div>}
+                        </TrackVisibility>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2}></img>
+        </section>
+    )
+
+}
