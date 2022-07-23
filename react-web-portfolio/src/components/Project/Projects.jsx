6cc37dbeb4cae56032cfda3eccb237f137d0aee2
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-23 02:10:53 -0300

    feat: Mudando estrutura de projetos

--- react-web-portfolio/src/components/Project/Projects.jsx
+++ react-web-portfolio/src/components/Project/Projects.jsx
@@ -1,42 +1,13 @@
-import { Container, Row, Col, Tab } from "react-bootstrap";
 import { ProjectCard } from "../ProjectCard/ProjectCard";
-import colorSharp2 from "../../assets/img/banners/color-sharp2.png";
-import { projects } from "../../utils/projects-constants"
+import { ProjectCardList } from "../ProjectCardList/ProjectCardList";
+import "../ProjectCardList/index.css";
 import './index.css';
+
 
 export const Projects = () => {
     return (
         <section className="project" id="projects">
-            <Container>
-                <Row>
-                    <Col size={12}>
-
-                        <h2>Projetos</h2>
-                        <p className="line"></p>
-                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Tab.Content id="slideInUp">
-                                <Tab.Pane eventKey="first">
-                                    <Row>
-                                        {
-                                            projects.map((project, index) => {
-                                                return (
-                                                    <ProjectCard
-                                                        key={index}
-                                                        {...project}
-                                                    />
-                                                )
-                                            })
-                                        }
-                                    </Row>
-                                </Tab.Pane>
-
-                            </Tab.Content>
-                        </Tab.Container>
-
-                    </Col>
-                </Row>
-            </Container>
-            <img className="background-image-right" src={colorSharp2}></img>
+            <ProjectCardList />
         </section>
     )
 
