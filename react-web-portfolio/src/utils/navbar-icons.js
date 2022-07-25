469283ce0c2ce51bfaf4d23fe24bc76688480534
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-25 12:36:49 -0300

    style: importanto icones

--- react-web-portfolio/src/utils/navbar-icons.js
+++ react-web-portfolio/src/utils/navbar-icons.js
@@ -1,9 +1,13 @@
 import navIcon1 from '../assets/img/navIcon/nav-icon1.svg';
 import navIcon3 from '../assets/img/navIcon/nav-icon3.svg';
 import navIcon4 from '../assets/img/navIcon/nav-icon4.svg';
+import navIcon5 from '../assets/img/navIcon/nav-icon5.svg';
+import navIcon6 from '../assets/img/navIcon/nav-icon6.svg';
 
 export const navbarIcons = {
     linkedin: navIcon1,
     instagram: navIcon3,
-    github: navIcon4
+    github: navIcon4,
+    whatsapp: navIcon5,
+    telegram: navIcon6
 }
