0e1140d6137f832a7154baad298ad064dc511a78
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-08-10 00:03:09 -0300

    refactor: atualizando constantes

--- react-web-portfolio/src/utils/projects-constants.js
+++ react-web-portfolio/src/utils/projects-constants.js
@@ -4,43 +4,54 @@ import groovooOrganizer from '../assets/img/projects/groovoo-organizer.jpeg';
 import concursoDeRedacao from '../assets/img/projects/dpu-concurso-de-redacao.png';
 import portalDpu from '../assets/img/projects/portal-dpu.png';
 import promocaoDeDireitosHumanos from '../assets/img/projects/promocao-de-direitos-humanos.png';
-import gaiadex from '../assets/img/projects/gaiadex.png';
 
 export const projects = [
     {
+        id: 1,
         title: 'Groovoo Ticketing',
         description: 'Desenvolvedor Full stack',
         imgUrl: groovooTicketing,
-        link: 'https://groovooapp.com/'
+        path: `/projects/1/?project=groovoo_ticketing`,
+        link: 'https://groovooapp.com/',
     },
     {
+        id: 2,
         title: 'Groovoo Social Networking',
         description: 'Desenvolvedor Full stack',
         imgUrl: groovooSocialNetworking,
+        path: '/project?=groovoo_social_networking',
         link: 'https://apps.apple.com/br/app/groovoo-social-networking/id1529056214'
     },
     {
+        id: 3,
         title: 'Groovoo Ticketing',
         description: 'Desenvolvedor Full stack',
         imgUrl: groovooOrganizer,
+        path: '/project?=groovoo_organizer',
         link: 'https://apps.apple.com/br/app/groovoo-organizer/id1593068649'
     },
     {
+        id: 4,
         title: 'Concurso de Redação da Defensoria Pública da União',
         description: 'Desenvolvedor fron-end',
         imgUrl: concursoDeRedacao,
+        path: '/project?=dpu_concurso_de_redacao',
         link: 'https://concursoderedacao.dpu.def.br/'
     },
     {
+        id: 5,
         title: 'Portal DPU',
         description: 'Desenvolvedor front end',
         imgUrl: portalDpu,
+        path: '/project?=dpu_portal',
         link: 'https://www.dpu.def.br/'
     },
     {
+        id: 6,
         title: 'Portal de Promoção de Direitos Humanos - DPU',
         description: 'Desenvolvedor Full stack',
         imgUrl: promocaoDeDireitosHumanos,
+        path: '/project?=dpu_direitos_humanos',
         link: 'https://promocaodedireitoshumanos.dpu.def.br/'
     }
 ]
