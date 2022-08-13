43bb84a0e4150e8940ece5064b037eba9e0dccad
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-08-12 23:47:47 -0300

    chore: atualizando constantes

--- react-web-portfolio/src/utils/projects-constants.js
+++ react-web-portfolio/src/utils/projects-constants.js
@@ -1,6 +1,18 @@
-import groovooTicketing from '../assets/img/projects/groovoo-ticketing.png';
-import groovooSocialNetworking from '../assets/img/projects/groovoo-social-networking.jpeg';
-import groovooOrganizer from '../assets/img/projects/groovoo-organizer.jpeg';
+import groovooTicketing1 from '../assets/img/projects/groovoo-ticketing-1.png';
+import groovooTicketing2 from '../assets/img/projects/groovoo-ticketing-2.png';
+import groovooTicketing3 from '../assets/img/projects/groovoo-ticketing-3.png';
+import groovooOrganizer1 from '../assets/img/projects/groovoo-organizer-1.png';
+import groovooOrganizer2 from '../assets/img/projects/groovoo-organizer-2.jpeg';
+import groovooOrganizer3 from '../assets/img/projects/groovoo-organizer-3.jpeg';
+import groovooOrganizer4 from '../assets/img/projects/groovoo-organizer-4.jpeg';
+import groovooOrganizer5 from '../assets/img/projects/groovoo-organizer-5.jpeg';
+import groovooSocial1 from '../assets/img/projects/groovoo-social-1.jpeg';
+import groovooSocial2 from '../assets/img/projects/groovoo-social-2.jpeg';
+import groovooSocial3 from '../assets/img/projects/groovoo-social-3.jpeg';
+import groovooSocial4 from '../assets/img/projects/groovoo-social-4.jpeg';
+import groovooSocial5 from '../assets/img/projects/groovoo-social-5.jpeg';
+import groovooSocial6 from '../assets/img/projects/groovoo-social-6.jpeg';
+
 import concursoDeRedacao from '../assets/img/projects/dpu-concurso-de-redacao.png';
 import portalDpu from '../assets/img/projects/portal-dpu.png';
 import promocaoDeDireitosHumanos from '../assets/img/projects/promocao-de-direitos-humanos.png';
@@ -9,14 +21,15 @@ import { ProjectSkillsConstants } from './project-skills-constants';
 export const projects = [
     {
         id: 1,
+        type: 'web',
         title: 'Groovoo Ticketing',
         acting: 'Desenvolvedor Full stack',
-        description: 'O Groovoo consiste em uma plataforma de venda de ingressos para eventos localizados nos Estados Unidos. Meu papel no desenvolvimento foi de maneira full stack com base no uso de TypeScript e ReactJs, além do uso de banco de dados relacionais e tecnologias como Firebase e Amazon AWS. ',
-        imgUrl: groovooTicketing,
+        description: 'O Groovoo Ticketing consiste é voltada para venda de ingressos para eventos localizados nos Estados Unidos, sendo disponível para usuaários normais e organizdores de eventos. Meu papel no desenvolvimento foi de maneira full stack com base no uso de TypeScript e ReactJs, além do uso de banco de dados relacionais e tecnologias como Firebase e Amazon AWS. ',
+        imgUrl: groovooTicketing1,
         imgs: [
-            groovooTicketing,
-            groovooTicketing,
-            groovooTicketing
+            groovooTicketing1,
+            groovooTicketing2,
+            groovooTicketing3
         ],
         skills: [
             ProjectSkillsConstants.node,
@@ -33,17 +46,54 @@ export const projects = [
     },
     {
         id: 2,
+        type: 'mobile',
         title: 'Groovoo Social Networking',
-        description: 'Desenvolvedor Full stack',
-        imgUrl: groovooSocialNetworking,
+        acting: 'Desenvolvedor Full stack',
+        description: 'O Groovoo Social Networking consiste em um aplicativo para usuários da plataforma de compra de ingressos Groovoo e seu funcionamento baseia-se em além de ser um mecanismo ágil para compra e armazenamento de ingressos, aproximar integrantes de determinados eventos por meio de interesses em comum. Meu papel no desenvolvimento foi de maneira full stack com base no uso de TypeScript e ReactNative',
+        imgUrl: groovooSocial1,
+        imgs: [
+            groovooSocial2,
+            groovooSocial3,
+            groovooSocial4,
+            groovooSocial5,
+            groovooSocial6,
+        ],
+        skills: [
+            ProjectSkillsConstants.node,
+            ProjectSkillsConstants.react,
+            ProjectSkillsConstants.typeScript,
+            ProjectSkillsConstants.jest,
+            ProjectSkillsConstants.mysql,
+            ProjectSkillsConstants.firebase,
+            ProjectSkillsConstants.docker,
+            ProjectSkillsConstants.git,
+        ],
         path: `/projects?project=groovoo_social_networking`,
         link: 'https://apps.apple.com/br/app/groovoo-social-networking/id1529056214'
     },
     {
         id: 3,
-        title: 'Groovoo Ticketing',
-        description: 'Desenvolvedor Full stack',
-        imgUrl: groovooOrganizer,
+        type: 'mobile',
+        title: 'Groovoo Organizer',
+        acting: 'Desenvolvedor Full stack',
+        description: 'O Groovoo Organizer consite em um aplicativo voltado para organizadores dos eventos criados dentro da plataforma Groovoo, seu objetivo é permitir que os organizadores tenham um fácil acesso a todos os dados do eventos. Meu papel no desenvolvimento foi com base no uso de TypeScript e ReactNative.',
+        imgUrl: groovooOrganizer1,
+        imgs: [
+            groovooOrganizer2,
+            groovooOrganizer3,
+            groovooOrganizer4,
+            groovooOrganizer5
+        ],
+        skills: [
+            ProjectSkillsConstants.node,
+            ProjectSkillsConstants.react,
+            ProjectSkillsConstants.typeScript,
+            ProjectSkillsConstants.jest,
+            ProjectSkillsConstants.mysql,
+            ProjectSkillsConstants.firebase,
+            ProjectSkillsConstants.docker,
+            ProjectSkillsConstants.git,
+        ],
         path: `/projects?project=groovoo_organizer`,
         link: 'https://apps.apple.com/br/app/groovoo-organizer/id1593068649'
     },
@@ -72,22 +122,3 @@ export const projects = [
         link: 'https://promocaodedireitoshumanos.dpu.def.br/'
     }
 ]
-
-export const responsive = {
-    superLargeDesktop: {
-        breakpoint: { max: 4000, min: 3000 },
-        items: 1
-    },
-    desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 1
-    },
-    tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 1
-    },
-    mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1
-    }
-};
---
+++ react-web-portfolio/src/utils/responsive-constants.js
@@ -0,0 +1,57 @@
+export const responsiveSkills = {
+    superLargeDesktop: {
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        slidesToSlide: 3
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
+
+export const responsiveProjectWeb = {
+    superLargeDesktop: {
+        breakpoint: { max: 4000, min: 3000 },
+        items: 1
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 1
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
+
+export const responsiveProjectMobile = {
+    superLargeDesktop: {
+        breakpoint: { max: 4000, min: 3000 },
+        items: 3
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 2
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 1
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
